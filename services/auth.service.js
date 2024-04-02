370d82e2bc80868a13c745568c5208a42d6d7995
Ian Clark <ianclark226@gmail.com>
2024-04-02 11:53:35 -0400

    template

---
+++ services/auth.service.js
@@ -0,0 +1,31 @@
+import { ServiceBroker } from "moleculer";
+
+const broker = new ServiceBroker();
+
+
+
+
+
+broker.createService({
+    name: 'auth',
+    actions: {
+            async authUser(ctx) {
+                const { username, password } = ctx.params
+
+                if(username === 'admin' && password === 'password') {
+                    return {
+                        success: true,
+                        message: 'Auth was successful'
+                    }
+                } else {
+                    return {
+                        success: false,
+                        message: 'Auth was unsuccessful'
+                    }
+                }
+            }
+        }
+    
+})
+
+export default broker;
